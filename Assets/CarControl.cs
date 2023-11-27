de860049591a7155227e05fa13f3a26330ff3f7e
Parric <liam.huppert@gmx.de>
2023-11-27 17:21:59 +0100

    mini

--- Assets/CarControl.cs
+++ Assets/CarControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CarControl : MonoBehaviour
 {
     float speed = 8f;
-    public float rotationSpeed = 2f;
+    public int rotationSpeed = 500;
 
     // Start is called before the first frame update
     void Start()
